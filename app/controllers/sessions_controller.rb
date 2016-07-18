2bbee4708768c87abe3b3f29aee7dd9323e30393
Ora-Kool <etingemabian@gmail.com>
2016-07-18 18:13:51 +0100

    Using the remember me system to remember users on prof-folio

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   def new
   end
 
+#Log the user in and redirect to the user's show page
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  		#Log the user in and redirect to the user's show page
   		log_in(user)
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
   		redirect_to user
   	else
   		flash.now[:danger] = 'Invalid email/password combination'
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out ? logged_in?
+  	log_out if logged_in?
     redirect_to root_url
   end
 end
